41ec7d93f0b337f55e2c502996d6b1367d3ca07f
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-12-17 13:35:04 +0300

    Update Loader3.java

--- src/Loader3.java
+++ src/Loader3.java
@@ -3,45 +3,66 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Loader3 {
     public static void main(String[] args) {
+        Logger logger = logFile();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите копируемый каталог");
-        String source = scanner.nextLine();
-
-        System.out.println("Введите в папку, в которую нужно скопировать");
-        String destination = scanner.nextLine();
-
+        logger.log(Level.INFO, "Введите адрес каталога");
+        String direction = scanner.nextLine();
+        logger.log(Level.INFO, "direction = " + direction);
+        logger.log(Level.INFO, "Введите адрес, куда нужно скопировать");
+        String receiver = scanner.nextLine();
+        logger.log(Level.INFO, "receiver = " + receiver);
         try {
-            copyDirectory(new File(source), new File(destination + source.substring(source.lastIndexOf("\\"))));
-            System.out.println("Копирование...");
+            copyDirectory(new File(direction), new File(receiver + direction.substring(direction.lastIndexOf("\\"))));
+            logger.log(Level.INFO, "Копирование...");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Произошла ошибка");
         }
     }
 
-    private static void copyDirectory(File source, File destination) throws IOException {
+    public static Logger logFile() {
+        Logger logger = Logger.getLogger(Loader3.class.getName());
 
-        if (source.isDirectory()) {
-            if (!destination.exists())
-                destination.mkdir();
+        FileHandler handler;
 
-            String[] files = source.list();
+        try {
+            handler = new FileHandler("log\\log.txt");
+            logger.addHandler(handler);
+            return logger;
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private static void copyDirectory(File direction, File receiver) throws IOException {
+
+        if (direction.isDirectory()) {
+            if (!receiver.exists())
+                receiver.mkdir();
+
+            String[] files = direction.list();
 
             if (files == null)
                 return;
 
             for (String file : files) {
-                File srcFile = new File(source, file);
-                File destFile = new File(destination, file);
-                copyDirectory(srcFile, destFile);
+                File dirFile = new File(direction, file);
+                File recFile = new File(receiver, file);
+                copyDirectory(dirFile, recFile);
             }
         } else {
+
             try {
-                FileInputStream in = new FileInputStream(source);
-                FileOutputStream out = new FileOutputStream(destination);
+                FileInputStream in = new FileInputStream(direction);
+                FileOutputStream out = new FileOutputStream(receiver);
                 byte[] buffer = new byte[1024];
                 int length;
                 while ((length = in.read(buffer)) > 0)
