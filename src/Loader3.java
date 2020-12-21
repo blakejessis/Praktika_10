9161296697be19513de123469e27250cfe92f6d3
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-12-21 16:16:42 +0300

    Update Loader3.java

--- src/Loader3.java
+++ src/Loader3.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 public class Loader3 {
     public static void main(String[] args) {
         Logger logger = logFile();
@@ -25,12 +24,10 @@ public class Loader3 {
             System.out.println("Произошла ошибка");
         }
     }
-
     public static Logger logFile() {
         Logger logger = Logger.getLogger(Loader3.class.getName());
 
         FileHandler handler;
-
         try {
             handler = new FileHandler("log\\log.txt");
             logger.addHandler(handler);
@@ -40,26 +37,19 @@ public class Loader3 {
             return null;
         }
     }
-
-
     private static void copyDirectory(File direction, File receiver) throws IOException {
-
         if (direction.isDirectory()) {
             if (!receiver.exists())
                 receiver.mkdir();
-
             String[] files = direction.list();
-
             if (files == null)
                 return;
-
             for (String file : files) {
                 File dirFile = new File(direction, file);
                 File recFile = new File(receiver, file);
                 copyDirectory(dirFile, recFile);
             }
         } else {
-
             try {
                 FileInputStream in = new FileInputStream(direction);
                 FileOutputStream out = new FileOutputStream(receiver);
@@ -67,7 +57,6 @@ public class Loader3 {
                 int length;
                 while ((length = in.read(buffer)) > 0)
                     out.write(buffer, 0, length);
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
